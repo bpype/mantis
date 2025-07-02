4f132f05ed935d385eb0fb2850f2476d7268c644
Joseph Brandenburg <josephbburg@protonmail.com>
2025-07-02 17:07:09 -0400

    v0.11.16

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ from .utilities import prRed
 
 MANTIS_VERSION_MAJOR=0
 MANTIS_VERSION_MINOR=11
-MANTIS_VERSION_SUB=15
+MANTIS_VERSION_SUB=16
 
 classLists = [module.TellClasses() for module in [
  link_definitions,
